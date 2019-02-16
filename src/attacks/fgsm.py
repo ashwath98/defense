c749f4435e0b031adc109f1b76290d5f4a231e2a
Ashwath Shetty <you@example.com>
2019-02-16 22:34:26 +0530

    Fixed Name Error Passes all tests

--- src/attacks/fgsm.py
+++ src/attacks/fgsm.py
@@ -5,7 +5,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 import numpy as np
-import matplotlib.pyplot as plt
 from abc import ABC, abstractmethod
 
 
@@ -18,9 +17,8 @@ class Attack(ABC):
                epsilon,
                y=None,
                targeted=False,
-               min_value,
-               max_value):
-
+               min_value=0,
+               max_value=1):
     self.model = model
     self.device = device
     self.data = data
@@ -32,6 +30,7 @@ class Attack(ABC):
 
   @abstractmethod
   def generate(self):
+
     pass
 
 
@@ -47,24 +46,26 @@ class FGSM(Attack):
                min_value=0,
                max_value=1):
     """
-    parameters:-
-    model :-The model under attack
-    device :- CPU or GPU acc to usage
-    data :- input image
-    epsilon :- value of the perturbation
-    y :- target /output labels
-    targeted :- targeted version of attack
+        parameters:-
+        model :-The model under attack
+        device :- CPU or GPU acc to usage
+        data :- input image
+        epsilon :- value of the perturbation
+        y :- target /output labels
+        targeted :- targeted version of attack
 
-    4 Cases are possible according to the combination of targeted and y variables
-    Case 1 :-y is specified and targeted is False .. then y is treated as the real output labels
-    Case 2 :-y is specified and targeted is True ... then the targeted version of the attack takes place and y is the target label
-    Case 3 :-y is None and targeted is False ... then the predicted outputs of the model are treated as the real outputs and the attack takes place
-    Case 4 :-y is None and targeted is True .. Invalid Input"""
-    super(FGSM, self).__init__(model, device, data, epsilon, y, targeted,
-                               min_value, max_value)
+        4 Cases are possible according to the combination of targeted and y variables
+        Case 1 :-y is specified and targeted is False .. then y is treated as the real output labels
+        Case 2 :-y is specified and targeted is True ... then the targeted version of the attack takes place and y is the target label
+        Case 3 :-y is None and targeted is False ... then the predicted outputs of the model are treated as the real outputs and the attack takes place
+        Case 4 :-y is None and targeted is True .. Invalid Input"""
+
+    super().__init__(model, device, data, epsilon, y, targeted, min_value,
+                     max_value)
 
   def fgsm_update(self, image, epsilon, data_grad):
     """Update the image with gradients of input"""
+
     sign_data_grad = data_grad.sign()
     perturbed_image = image + epsilon * sign_data_grad
     perturbed_image = torch.clamp(perturbed_image, self.min_value,
@@ -72,24 +73,26 @@ class FGSM(Attack):
     return perturbed_image
 
   def generate(self):
+
     if self.targeted == True and self.y is None:
+
       print("label should be specified when targeted is true")
       return -1, -1, -1
 
     if self.y is not None and type(self.y) != torch.Tensor:
-      #if input label is not torch tensor convert it to  one
+
       self.y = torch.Tensor([self.y]).type(torch.int64)
 
-    data = self.data.to(device)
+    data = self.data.to(self.device)
     data.requires_grad = True
-    output = model(data)
+    output = self.model(data)
     init_pred = output.max(1, keepdim=True)[1]
     if self.y is None:
       # if no y is specified use predictions as the label for the attack
       target = init_pred.view(1)
     else:
-      target = self.y  #use y itself as the target
-    target = target.to(device)
+      target = self.y  # use y itself as the target
+    target = target.to(self.device)
     loss = F.nll_loss(output, target)
     if self.targeted:
       loss = -loss
