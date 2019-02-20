f1e4c5592185182dfd937cae17febf5fa264a92b
Amit Jindal <amitj646@gmail.com>
2019-02-21 00:47:27 +0530

    Added PGD attack

--- src/attacks/attacks.py
+++ src/attacks/attacks.py
@@ -6,7 +6,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 import numpy as np
 from abc import ABC, abstractmethod
-
+from torch.autograd import Variable
 
 class Attack(ABC):
 
@@ -81,3 +81,53 @@ class FGSM(Attack):
     perturbed_image = torch.clamp(perturbed_image, self.min_value,
                                   self.max_value)
     return init_pred, perturbed_image
+
+class PGD(Attack):
+    def __init__(self, model, device, iters=40, step_size=0.01, 
+        random_start=True):
+      
+        self.iters = iters
+        self.step_size = step_size
+        self.rand = random_start
+        self.loss_fn = nn.CrossEntropyLoss()
+
+        super().__init__(model, device)
+
+    def perturb(self, X_img, epsilon, y=None):
+        if self.rand:
+            X = X_img.cpu().numpy() + np.random.uniform(-epsilon, epsilon, X_img.cpu().numpy().shape).astype('float32')
+        else:
+            X = np.copy(X)
+
+        data = torch.from_numpy(X)
+        data = data.to(self.device)
+        data.requires_grad = True
+        output = self.model(data)
+        init_pred = output.max(1, keepdim=True)[1]
+        if y is None:
+          # if no y is specified use predictions as the label for the attack
+          target = init_pred.view(1)
+        else:
+          target = y  # use y itself as the target
+
+        y = y.cpu().numpy()
+        for i in range(self.iters):
+    
+          y_var = Variable(torch.LongTensor(y))
+          X_var = torch.from_numpy(X)
+          X_var = X_var.to(self.device)
+          y_var = y_var.to(self.device)
+          X_var.requires_grad = True
+          scores = self.model(X_var)
+          loss = self.loss_fn(scores, y_var)
+          self.model.zero_grad()
+          loss.backward()
+          grad = X_var.grad.data.cpu().numpy()
+          X += self.step_size * np.sign(grad)
+
+          X = np.clip(X, X_img.cpu().numpy() - epsilon, X_img.cpu().numpy() + epsilon)
+          X = np.clip(X, 0, 1) # ensure valid pixel range
+        perturbed_image = torch.from_numpy(X)
+        perturbed_image = perturbed_image.to(self.device)
+
+        return init_pred, perturbed_image
